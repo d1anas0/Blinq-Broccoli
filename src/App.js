f084ed4b2220cc59665d5422895645a37a981468
d1anas0 <231689@gmail.com>
2023-05-02 18:21:12 +1000

    style landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
@@ -13,19 +14,31 @@ export default function App() {
   return (
     <>
       <Header />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+        style={{
+          height: "100vh",
+          padding: "6%",
+        }}
+        backgroundColor="#9FBF9B"
+        align="center"
+      >
+        {/* heading */}
+        <Typography variant="h2" fontWeight="medium" color="#121113">
+          A better way to enjoy every day.
+        </Typography>
 
-      {/* heading */}
-      <Typography variant="h2" fontWeight="medium" color="#121113">
-        A better way to enjoy every day.
-      </Typography>
+        {/* small piece of text */}
+        <Typography variant="h5" marginTop="4%" color="#121113">
+          Be the first to know when we launch.
+        </Typography>
 
-      {/* small piece of text */}
-      <Typography variant="h5" marginTop="4%" color="#121113">
-        Be the first to know when we launch.
-      </Typography>
-
-      {/* REQUEST INVITE BUTTON */}
-
+        {/* REQUEST INVITE BUTTON */}
+      </Box>
       <Footer />
     </>
   );
