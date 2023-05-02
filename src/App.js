e00deb6b1674baa7cb7fe7118fb48b7c6e183893
d1anas0 <231689@gmail.com>
2023-05-02 16:58:55 +1000

    add content (App landing page)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
+import Typography from "@mui/material/Typography";
+
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+
+// primary colour = #9FBF9B
+// secondary colour = #121113
+// text colour = #E0DDCF
+// text colour = #EFEDE6
 
 export default function App() {
   return (
     <>
       <Header />
+
+      {/* heading */}
+      <Typography variant="h2" fontWeight="medium" color="#121113">
+        A better way to enjoy every day.
+      </Typography>
+
+      {/* small piece of text */}
+      <Typography variant="h5" marginTop="4%" color="#121113">
+        Be the first to know when we launch.
+      </Typography>
+
+      {/* REQUEST INVITE BUTTON */}
+
       <Footer />
     </>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+
 import { Header } from "./components/Header";
 
 test("check brand name is correct in Header", () => {
