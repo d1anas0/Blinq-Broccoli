bc28386ce56d49ec722538fc8b111343467fbe7b
d1anas0 <231689@gmail.com>
2023-05-02 15:07:15 +1000

    style component (Header)

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,5 +2,19 @@ import React from "react";
 import AppBar from "@mui/material/AppBar";
 
 export function Header() {
-  return <AppBar position="sticky">Broccoli & Co.</AppBar>;
+  return (
+    <AppBar
+      position="sticky"
+      sx={{
+        backgroundColor: "#121113",
+        typography: "h6",
+        textAlign: "left",
+        texTransform: "uppercase",
+        color: "#9FBFB9B",
+      }}
+      style={{ padding: 10, paddingLeft: 20 }}
+    >
+      Broccoli & Co.
+    </AppBar>
+  );
 }
