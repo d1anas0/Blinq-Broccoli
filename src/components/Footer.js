8613d459b1fbcf99ec3e83160fe97f0eb5dbc6b4
d1anas0 <231689@gmail.com>
2023-05-02 16:49:15 +1000

    add content (Footer)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,28 @@
 import React from "react";
 import AppBar from "@mui/material/AppBar";
+import Typography from "@mui/material/Typography";
 
 export function Footer() {
   return (
     <AppBar
-      position="fixed"
+      position="static"
       sx={{
-        backgroundColor: "red",
+        backgroundColor: "#121113",
       }}
     >
-      This is the Footer
+      <Typography variant="body2" align="center" color="#EFEDE6">
+        Made with{" "}
+        <span role="img" aria-label="love">
+          ❤️
+        </span>{" "}
+        in Melbourne
+      </Typography>
+      <Typography variant="body2" align="center" color="#EFEDE6">
+        <span role="img" aria-label="copyright">
+          ©️{" "}
+        </span>
+        Broccoli & Co. All rights reserved.
+      </Typography>
     </AppBar>
   );
 }
