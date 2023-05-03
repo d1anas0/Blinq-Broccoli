776bb22d13c5f2d88c437f78b519189771901d48
d1anas0 <231689@gmail.com>
2023-05-03 16:53:01 +1000

    add button (landing page)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
@@ -27,17 +28,24 @@ export default function App() {
         backgroundColor="#9FBF9B"
         align="center"
       >
-        {/* heading */}
         <Typography variant="h2" fontWeight="medium" color="#121113">
           A better way to enjoy every day.
         </Typography>
-
-        {/* small piece of text */}
         <Typography variant="h5" marginTop="4%" color="#121113">
           Be the first to know when we launch.
         </Typography>
 
         {/* REQUEST INVITE BUTTON */}
+        <Box style={{ marginTop: "4%" }}>
+          <Button
+            variant="contained"
+            size="large"
+            style={{ backgroundColor: "#db7f50", color: "#121113" }}
+            onClick={() => alert("clicked!")}
+          >
+            request an invite
+          </Button>
+        </Box>
       </Box>
       <Footer />
     </>
