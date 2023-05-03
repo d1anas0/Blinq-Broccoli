05f288917030541903946ca50cb9cd4687c0ea44
d1anas0 <231689@gmail.com>
2023-05-03 16:11:13 +1000

    add tests (Landing page, form modal)

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import { render, screen } from "@testing-library/react";
 
-import { Header } from "./components/Header";
+import App from "./App";
 
-test("check brand name is correct in Header", () => {
-  render(<Header />);
-  const brandName = screen.getByText("Broccoli & Co.");
-  expect(brandName).toBeInTheDocument();
+describe("In the Landing Page", () => {
+  describe("Request Invite Form", () => {
+    describe("Given Landing Page DID render correctly", () => {
+      it("Should find the 'Request an Invite' button", () => {
+        render(<App />);
+        expect(screen.getByRole("button")).toBeInTheDocument();
+      });
+    });
+    describe("Given the 'Request Invite Form' did NOT open", () => {
+      it("Should NOT display the form", () => {
+        render(<Modal closeModal />);
+        expect(screen.queryByRole("form")).toBeNull();
+      });
+    });
+    describe("Given the 'Request Invite Form' DID open", () => {
+      it("Should find the 'Send' button", () => {
+        render(<Modal open />);
+        expect(screen.getByText(/send/i)).toBeInTheDocument();
+      });
+    });
+  });
 });
