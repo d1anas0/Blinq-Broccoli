d18cdcd6a22a5f48153864c4ce348ab9cffb3ec7
d1anas0 <231689@gmail.com>
2023-05-04 17:25:50 +1000

    (WIP): Tests... really testing me now!

--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,34 @@
-import { render, screen, fireEvent, cleanup } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 import App from "./App";
-import FormModal from "./components/FormModal";
 
-afterEach(cleanup);
-
-describe("In the Landing Page", () => {
-  describe("Request Invite Form", () => {
-    describe("Given Landing Page DID render correctly", () => {
-      it("Should find the 'Request an Invite' button", () => {
-        render(<App />);
-        expect(screen.getByRole("button")).toBeInTheDocument();
-      });
-    });
-    //   it("The button should call a function to open the 'Request Invite Form'", () => {
-    //     render(<App />);
-    //     const openModalSpy = jest.fn();
-    //     fireEvent.click(screen.getByRole("button"));
-    //     expect(openModalSpy).toHaveBeenCalled();
-    //   });
-    // it("Should find the 'Send' button", () => {
-    //   // render(<FormModal openModal={isVisible} />);
-    //   expect(screen.getByText(/send/i)).toBeInTheDocument();
-    // });
+// 1. Render App
+// 2. Assert that I don't see any popups/modals by default
+describe("Given Landing Page DID render correctly", () => {
+  //   it("Should NOT be displaying any modals (THIS SHOULD FAIL)", () => {
+  //     render(<App />);
+  //     expect(screen.queryByText("Full Name")).not.toBeInTheDocument();
+  //   });
+  // });
+  // 3. Find the "Invite" button
+  // describe("Given Landing Page DID render correctly", () => {
+  it("Should find a button", () => {
+    render(<App />);
+    expect(screen.getByRole("button")).toBeInTheDocument();
   });
 });
+
+// 4. Click on it
+// 5. Assert that I now see the modal
+// describe("After clicking on 'Request an Invite' button", () => {
+//   it("Should display a modal titled 'Request an Invite'", () => {
+//     render(<App />);
+//     fireEvent.click(screen.getByRole("button"));
+//     expect(screen.getByTestId("dialog-title").textContent).toBe(
+//       "Request an Invite"
+//     );
+//   });
+// });
+
+// 6. Click outside it
+// 7. Assert I can no longer see the modal
