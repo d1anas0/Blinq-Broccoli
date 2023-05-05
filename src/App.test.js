bef0c9f7a2b1c10de3928ef9073c35af5edf81b1
d1anas0 <231689@gmail.com>
2023-05-05 22:14:08 +1000

    (WIP): Fixed tests

--- src/App.test.js
+++ src/App.test.js
@@ -5,30 +5,33 @@ import App from "./App";
 // 1. Render App
 // 2. Assert that I don't see any popups/modals by default
 describe("Given Landing Page DID render correctly", () => {
-  //   it("Should NOT be displaying any modals (THIS SHOULD FAIL)", () => {
-  //     render(<App />);
-  //     expect(screen.queryByText("Full Name")).not.toBeInTheDocument();
-  //   });
+  it("Should NOT be displaying any modals", () => {
+    render(<App />);
+    expect(screen.queryByText(/full Name/i)).not.toBeInTheDocument();
+  });
   // });
+  // _________________________________________
   // 3. Find the "Invite" button
   // describe("Given Landing Page DID render correctly", () => {
-  it("Should find a button", () => {
+  it("Should find the 'Request an Invite' button", () => {
     render(<App />);
-    expect(screen.getByRole("button")).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: /request an invite/i })
+    ).toBeInTheDocument();
   });
 });
-
+// _________________________________________
 // 4. Click on it
 // 5. Assert that I now see the modal
-// describe("After clicking on 'Request an Invite' button", () => {
-//   it("Should display a modal titled 'Request an Invite'", () => {
-//     render(<App />);
-//     fireEvent.click(screen.getByRole("button"));
-//     expect(screen.getByTestId("dialog-title").textContent).toBe(
-//       "Request an Invite"
-//     );
-//   });
-// });
-
+describe("After clicking on 'Request an Invite' button", () => {
+  it("Should display a modal titled 'Request an Invite'", () => {
+    render(<App />);
+    fireEvent.click(screen.getByRole("button", { name: /request an InVite/i }));
+    expect(screen.getByTestId("dialog-title").textContent).toBe(
+      "Request an Invite"
+    );
+  });
+});
+// _________________________________________
 // 6. Click outside it
 // 7. Assert I can no longer see the modal
