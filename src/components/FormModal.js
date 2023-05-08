d08449be517f2ebe123451903d66310fb3ad0bbc
d1anas0 <231689@gmail.com>
2023-05-08 12:54:58 +1000

    add confirm email check

--- src/components/FormModal.js
+++ src/components/FormModal.js
@@ -7,37 +7,32 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
 export function FormModal({ openModal, closeModal }) {
-  // get email input
   const [emailValue, setEmailValue] = useState("");
+  const [confirmEmailValue, setConfirmEmailValue] = useState("");
+  const [emailFormatError, setEmailFormatError] = useState(false);
+  const [emailMatchError, setEmailMatchError] = useState(false);
+
   const handleEmailValue = (input) => {
     setEmailValue(input.target.value);
   };
+  const handleConfirmEmailValue = (input) => {
+    setConfirmEmailValue(input.target.value);
+  };
 
   // Email Validation - applying `inputProps={{ pattern: xxx }}` attribute to <TextField> does not work.
-  const [emailError, setEmailError] = useState(false);
   const validateEmail = (e) => {
     e.preventDefault();
     const emailFormat = /[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/.test(
       emailValue
     );
-    !emailFormat ? setEmailError(true) : setEmailError(false);
-    console.log(emailFormat);
+    !emailFormat ? setEmailFormatError(true) : setEmailFormatError(false);
+    console.log("is email format conforming", emailFormat);
+
+    // Confirm email matches
+    const confirmEmail = emailValue === confirmEmailValue;
+    !confirmEmail ? setEmailMatchError(true) : setEmailMatchError(false);
+    console.log("does email match : ", confirmEmail);
   };
-
-  //___________________________________________________________
-
-  // Confirm email matches
-
-  // get confirm email input
-  // const [confirmEmailValue, setConfirmEmailValue] = useState("");
-  // const handleConfirmEmailValue = (input) => {
-  //   setConfirmEmailValue(input.target.value);
-  //   console.log(`Confirm email value = ${input.target.value}`);
-  // };
-  // const checkEmailMatches =
-  //   emailValue === confirmEmailValue
-  //     ? () => alert("submitted!")
-  //     : () => alert("try again!");
 
   return (
     <Dialog maxWidth="xs" open={openModal} onClose={closeModal}>
@@ -66,12 +61,13 @@ export function FormModal({ openModal, closeModal }) {
           //
           component="form"
         >
-          <Grid container spacing={2}>
+          <Grid container spacing={4}>
             <Grid item xs={12}>
               <TextField
                 inputProps={{ minLength: 3 }}
                 helperText="Minimum 3 characters"
                 // error
+
                 required
                 fullWidth
                 type="text"
@@ -84,7 +80,7 @@ export function FormModal({ openModal, closeModal }) {
             <Grid item xs={12}>
               <TextField
                 helperText="Required format = abc123@mailbox.com"
-                error={emailError}
+                error={emailFormatError}
                 required
                 fullWidth
                 type="email"
@@ -94,15 +90,14 @@ export function FormModal({ openModal, closeModal }) {
                 autoComplete="email"
                 value={emailValue}
                 onChange={handleEmailValue}
-                // Note: pattern attribute does not work? Browser responds with error message which I can't relate my code to >.<"
-                // inputProps={{
-                //   pattern: "[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}",
-                // }}
+
                 // Would consider using eg. React-Hook-Form for more involved/complex forms
               />
             </Grid>
-            {/* <Grid item xs={12}>
-              {/* <TextField
+            <Grid item xs={12}>
+              <TextField
+                helperText="Emails must match"
+                error={emailMatchError}
                 required
                 fullWidth
                 type="email"
@@ -113,7 +108,7 @@ export function FormModal({ openModal, closeModal }) {
                 value={confirmEmailValue}
                 onChange={handleConfirmEmailValue}
               />
-            </Grid> */}
+            </Grid>
           </Grid>
           <Button
             fullWidth
@@ -121,10 +116,7 @@ export function FormModal({ openModal, closeModal }) {
             variant="contained"
             style={{ backgroundColor: "#199059", color: "#121113" }}
             sx={{ my: "15%" }}
-            onClick={
-              validateEmail
-              //&& checkEmailMatches
-            }
+            onClick={validateEmail}
           >
             Send
           </Button>
